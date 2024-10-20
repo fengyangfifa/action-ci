a8115316ed754539c65e924cb02b363fae01d508
fengyangfifa <2314034614@qq.com>
2024-10-20 23:02:01 +0800

    ci/cd workflows deploy.

--- src/app.tsx
+++ src/app.tsx
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <div>
-      hello github pages test ss
+      hello github pages test sss
       <p className="count">{count}</p>
       <img src={web} alt="web" />
       <p>{testTxt}</p>
